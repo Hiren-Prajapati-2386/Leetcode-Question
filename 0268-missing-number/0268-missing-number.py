25a9e93a4122e8a61c3547a01c08efbbe1460a15
Hiren Prajapati <hirenprajapati2386@gmail.com>
2026-06-30 17:56:58 +0530

    Time: 0 ms (100%), Space: 21 MB (6.73%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -2,6 +2,7 @@ class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         start = 0
         end  = len(nums)
+        nums = set(nums)
         
         for i in range(start,end+1):
             if i not in nums:
@@ -9,3 +10,4 @@ class Solution:
 
         return -1
         
+# 0 1 2 3 4 5 6 7 _ 9
