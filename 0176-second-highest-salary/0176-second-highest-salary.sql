3c30395b36eb9847c8080971515647408a867211
Hiren Prajapati <hirenprajapati2386@gmail.com>
2026-08-21 00:11:33 +0530

    Time: 311 ms (39.09%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,8 +1,11 @@
-# Write your MySQL query statement below
+
+with second_highest as (
+    select salary as SecondHighestSalary, dense_rank() over(order by salary desc) as rn
+    from Employee
+)
 
 select (
-select distinct salary from Employee
-where salary not in (select max(salary) from Employee)
-order by salary desc
+select SecondHighestSalary from second_highest
+where rn = 2
 limit 1
 ) as SecondHighestSalary;
