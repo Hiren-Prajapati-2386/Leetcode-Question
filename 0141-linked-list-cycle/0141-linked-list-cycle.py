a1f90a5a4a30d02a9d56da0ebf7c206a0fa1a005
Hiren Prajapati <hirenprajapati2386@gmail.com>
2026-09-15 22:34:56 +0530

    Time: 60 ms (19.25%), Space: 22.4 MB (97.9%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -4,19 +4,43 @@
 #         self.val = x
 #         self.next = None
 
+# class Solution:
+#     def hasCycle(self, head: Optional[ListNode]) -> bool:
+#         # use set insterd map becuse serching eliment take o(1) time 
+#         seen = set()
+
+#         point = head
+
+#         while(point is not None):
+#             if point in seen:
+#                 return True
+#             seen.add(point)
+
+#             point = point.next
+
+        
+#         return False
+
+
+        # time complecity :- O(n)
+        # space complecity is also O(n)  so this is good but not whatleetcode ask
+
+
+        
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        seen = set()
+        # Floyd’s Cycle-Finding Algorithm
 
-        point = head
+        # we have two pointer fast and slow
+        fast = head
+        slow = head
 
-        while(point is not None):
-            if point in seen:
+        while fast is not None and fast.next is not None:
+
+            fast = fast.next.next
+            slow = slow.next
+
+            if fast == slow:
                 return True
-            seen.add(point)
 
-            point = point.next
-
-        
         return False
-        
